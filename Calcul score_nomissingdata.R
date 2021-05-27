9731fcf49c8c505be0d85e6e99a3f13144224845
SuzanneCl <suzanne.clougher@gmail.com>
2021-05-27 18:04:21 +0200

    Created a matrix with score info to reduce code length

--- Calcul score_nomissingdata.R
+++ Calcul score_nomissingdata.R
@@ -115,57 +115,47 @@ ggplot(table_scores) +
   geom_histogram(alpha = 0.5, position = "identity", bins = 22) +
   labs(x = "WCRF/AICR score", title = "WCRF/AICR scores in the E3N cancer group") 
 
-#table_scores2 <- df.scores %>%
-#  select(CT, sc.BMI,  sc.TT, sc.PA, sc.FV, sc.TDF, sc.UPF, sc.MEAT, sc.SD, sc.ALC, sc.BFD, score) %>%
-#  mutate(CT = factor(CT, levels = c("0", "1"), labels = c("Controls", "Cases")))
-#scores_fct <- table_scores2 %>%
-#  mutate(CT = as.factor(CT), sc.BMI = as.factor(sc.BMI), sc.TT = as.factor(sc.TT),sc.PA = as.factor(sc.PA), sc.FV = as.factor(sc.FV), sc.TDF = as.factor(sc.TDF), sc.UPF = as.factor(sc.UPF), sc.MEAT = as.factor(sc.MEAT), sc.SD = as.factor(sc.SD), sc.ALC = as.factor(sc.ALC), sc.BFD = as.factor(sc.BFD))
+# Ceate a matrix containing score information
+matrix_scores <- data.matrix(table_scores)
 
+# Table with score decomposition
+score_decompCTR <- df.scores %>%
+  filter (CT == 0) %>%
+  select(sc.BMI,  sc.TT, sc.PA, sc.FV, sc.TDF, sc.UPF, sc.MEAT, sc.SD, sc.ALC, sc.BFD, score) %>%
+  mutate(sc.BMI = as.factor(sc.BMI), sc.TT = as.factor(sc.TT),sc.PA = as.factor(sc.PA), sc.FV = as.factor(sc.FV), sc.TDF = as.factor(sc.TDF), sc.UPF = as.factor(sc.UPF), sc.MEAT = as.factor(sc.MEAT), sc.SD = as.factor(sc.SD), sc.ALC = as.factor(sc.ALC), sc.BFD = as.factor(sc.BFD))
+summary(score_decompCTR)
+
+score_decompCS <- df.scores %>%
+  filter (CT == 1) %>%
+  select(sc.BMI,  sc.TT, sc.PA, sc.FV, sc.TDF, sc.UPF, sc.MEAT, sc.SD, sc.ALC, sc.BFD, score) %>%
+  mutate(sc.BMI = as.factor(sc.BMI), sc.TT = as.factor(sc.TT),sc.PA = as.factor(sc.PA), sc.FV = as.factor(sc.FV), sc.TDF = as.factor(sc.TDF), sc.UPF = as.factor(sc.UPF), sc.MEAT = as.factor(sc.MEAT), sc.SD = as.factor(sc.SD), sc.ALC = as.factor(sc.ALC), sc.BFD = as.factor(sc.BFD))
+summary(score_decompCS)
 
 # Test Student - Score components ----------------------------------------------------------------------
 
 # Checking variables normal distribution
+normal_distrib_comp <- function (x) {
+  qqnorm(matrix_scores[,x], main =colnames(matrix_scores)[x])
+  qqline(matrix_scores[,x])
+}
 
-#BMI
-qqnorm(table_scores$BMI, main = 'BMI')
-qqline(table_scores$BMI)
-#Waist circumference
-qqnorm(table_scores$TTAILLE, main = 'Waist circumference')
-qqline(table_scores$TTAILLE)
-#Physical activity
-qqnorm(table_scores$TotalAPQ3, main = 'Physical activity')
-qqline(table_scores$TotalAPQ3)
-#Fruits and vegetables
-qqnorm(table_scores$fruitveg, main = 'Fruits & vegetables')
-qqline(table_scores$fruitveg)
-#Total fiber consumption
-qqnorm(table_scores$TDF, main = 'Fiber')
-qqline(table_scores$TDF)
-#Red meat
-qqnorm(table_scores$Rmeat, main = 'Red meat')
-qqline(table_scores$Rmeat)
-#Processed meat
-qqnorm(table_scores$Pmeat, main = 'Processed meat')
-qqline(table_scores$Pmeat)
-#Percentage of aUPF
-qqnorm(table_scores$percent_aUPF, main = 'aUPF')
-qqline(table_scores$percent_aUPF)
-#Sugary drinks
-qqnorm(table_scores$sugary_drinks, main = 'Sugary drinks')
-qqline(table_scores$sugary_drinks)
-#Alcohol
-qqnorm(table_scores$ALCOHOL, main = 'Ethanol')
-qqline(table_scores$ALCOHOL)
-#Breastfeeding
-qqnorm(table_scores$allaitement_dureecum, main = 'Durée cumulée allaitement')
-qqline(table_scores$allaitement_dureecum)
-#wCRF/AICR score
-qqnorm(table_scores$score, main = 'WCRF/AICR score')
-qqline(table_scores$score)
+for(i in c(2:13)){normal_distrib_comp(i)}
 
-
-t.test(BMI ~ CT, data = table_scores)
+# Test F de comparaison et test de Student apparié
 var.test(BMI ~ CT, data = table_scores)
+t.test(BMI ~ CT, data = table_scores)
+
+stud <- function (x) {
+  t.test(matrix_scores[,x] ~ CT, data = matrix_scores)
+}
+
+stud(14)
+
+ftest <- function (x) {
+  var.test(matrix_scores[,x] ~ CT, data = matrix_scores)
+}
+
+ftest(10)
 
 # Graphs for data distribution ---------------------------------------------------------------------
 
