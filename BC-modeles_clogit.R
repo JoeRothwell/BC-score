94314b6db0b9d27c8259e696f6dd984dc9c951df
SuzanneCl <suzanne.clougher@gmail.com>
2021-06-16 17:39:19 +0200

    initial commit

---
+++ BC-modeles_clogit.R
@@ -0,0 +1,10 @@
+# Case-control breast cancer study
+
+source("BC-prep_data_calc_score.R")
+library(survival)
+
+
+# Modeles score-breast cancer ---------------------------------------------------------------------
+
+# All participants
+mod1 <- clogit(CT ~ score + strata(MATCH), data = metabolo)
