1d6af5ff0a8d7fecfea5e6483c6486e3f763c60c
SuzanneCl <suzanne.clougher@gmail.com>
2021-07-07 10:43:34 +0200

    initial commit

---
+++ E3N-modeles_cox.R
@@ -0,0 +1,24 @@
+# E3N cohort
+# Graphs for score distribution and checking variables' distribution
+source("E3N-prep_data_calc_score.R")
+library("survival")
+
+# -------  Trying cox regression models
+data("lung")
+head(lung)
+# univariate cox model
+res.cox <- coxph(Surv(time, status) ~ sex, data=lung)
+res.cox
+summary(res.cox)
+# multivariate cox model
+multicox <- coxph(Surv(time, status) ~ sex + ph.ecog + wt.loss, data=lung)
+summary(multicox)
+
+# ------ end of tests
+
+# Univariate cox model
+coxmod1 <- coxph(Surv(duree_suivi_1, ksein) ~ score, data=df.scores_all)
+summary(coxmod1)
+
+coxmod2 <- coxph(Surv(duree_suivi_1, ksein) ~ score_cat, data=df.scores_all)
+summary(coxmod2)
