292113c832b98ae6aa1c82218e136139afbd30e5
SuzanneCl <suzanne.clougher@gmail.com>
2021-07-15 17:38:27 +0200

    new model

--- E3N-modeles_cox.R
+++ E3N-modeles_cox.R
@@ -3,7 +3,7 @@
 source("E3N-prep_data_calc_score.R")
 library("survival")
 
-# -------  Trying cox regression models
+# TESTS ------------------------------------  Trying cox regression models
 data("lung")
 head(lung)
 # univariate cox model
@@ -14,12 +14,15 @@ summary(res.cox)
 multicox <- coxph(Surv(time, status) ~ sex + ph.ecog + wt.loss, data=lung)
 summary(multicox)
 
-# ------ end of tests
+# END OF TESTS ----------------------------------------------
 
 # Univariate cox model
 coxmod1 <- coxph(Surv(duree_suivi_1, ksein) ~ score, data=df.scores_all)
-summary(coxmod1)
 
-df.scores_all$score_cat <- as.factor(df.scores_all$score_cat)
 coxmod2 <- coxph(Surv(duree_suivi_1, ksein) ~ score_cat, data=df.scores_all)
+
+coxmod3 <- coxph(Surv(duree_suivi_1, ksein) ~ score + bacfemme2, data=df.scores_all)
+
+summary(coxmod1)
 summary(coxmod2)
+summary(coxmod3)
