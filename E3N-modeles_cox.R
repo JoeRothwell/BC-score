056c4bdf148daba9a6d59f768879c335fbd016a5
SuzanneCl <suzanne.clougher@gmail.com>
2021-07-23 11:06:49 +0200

    removed test models

--- E3N-modeles_cox.R
+++ E3N-modeles_cox.R
@@ -3,19 +3,6 @@
 source("E3N-prep_data_calc_score.R")
 library("survival")
 
-# TESTS ------------------------------------  Trying cox regression models
-data("lung")
-head(lung)
-# univariate cox model
-res.cox <- coxph(Surv(time, status) ~ sex, data=lung)
-res.cox
-summary(res.cox)
-# multivariate cox model
-multicox <- coxph(Surv(time, status) ~ sex + ph.ecog + wt.loss, data=lung)
-summary(multicox)
-
-# END OF TESTS ----------------------------------------------
-
 # Univariate cox model
 coxmod1 <- coxph(Surv(duree_suivi_1, ksein) ~ score, data=df.scores_all)
 
