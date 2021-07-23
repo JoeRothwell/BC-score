8d3e84c303493aa5cbd9ab12d527ba13a89e6205
SuzanneCl <suzanne.clougher@gmail.com>
2021-07-23 20:07:01 +0200

    writing plsr models

--- BC-PLS_models.R
+++ BC-PLS_models.R
@@ -2,6 +2,8 @@
 # PLS models
 
 source("BC-prep_data_calc_score.R")
+
+# tests ------------
 library(pls)
 data(yarn)
 data(oliveoil)
@@ -9,8 +11,8 @@ data(gasoline)
 
 options(digits = 4)
 view(head(gasoline))
-gasTrain <- gasoline[1:50,]
-gasTest <- gasoline[51:60,]
+gasTrain <- gasoline[1:50,] #first part of the data used to create the model
+gasTest <- gasoline[51:60,] #second part left alone to see if the predictions with the model are ok
 
 # model with 10 components, includes leave-one-out cross-validated predictions
 gas1 <- plsr(octane ~ NIR, ncomp= 10, data = gasTrain, validation = "LOO")
@@ -20,6 +22,15 @@ plot(gas1, ncomp = 2, asp = 1, line = TRUE) #cross-validated predictions for the
 plot(gas1, plottype = "score", comps = 1:3) #score plot for the gasoline data
 explvar(gas1) #extraction of the explained variances
 
+# end tests ------------
+
 # Create a single table with score + metabolite information
 metabolo_df <- as.data.frame(metabolo)
-scores <- df.scores %>% pull(score)
+scores <- df.scores %>% select(score) 
+metab_scores <- bind_cols(scores, metabolo_df) 
+
+metab_names <- c(colnames(metabolo_df))
+
+modplsr <- plsr(score ~., data = metab_scores, validation = "LOO")
+summary(modplsr)
+plot(RMSEP(modplsr, legendpos = "topright"))
