c545f0ba1c32d38768520b60f622ebe275fa3f56
SuzanneCl <suzanne.clougher@gmail.com>
2021-07-27 15:25:23 +0200

    created single tables for data presentation

--- E3N-modeles_cox.R
+++ E3N-modeles_cox.R
@@ -4,26 +4,38 @@ source("E3N-prep_data_calc_score.R")
 library("survival")
 
 # Univariate cox model
-
+# bacfemme2 = niveau d'education
+# statfin_dquest=statut menopausique a la fin du questionnaire
 coxmod1 <- coxph(Surv(duree_suivi1, ksein) ~ score, data=df.scores_all)
-#adjusting for education level
 coxmod2 <- coxph(Surv(duree_suivi1, ksein) ~ score + bacfemme2, data=df.scores_all)
-#adjusting for education level and menopausal status
 coxmod3 <- coxph(Surv(duree_suivi1, ksein) ~ score + bacfemme2 + statfin_dquest, data=df.scores_all)
 
-summary(coxmod1)
-summary(coxmod2)
-summary(coxmod3)
+coxmod_list <- list(coxmod1, coxmod2, coxmod3) #models with score - all participants
+coxmod_names <- c("no adjustment variables", "+ education level", "+ menopause")
+
+library(broom)
+tablecox <- map_df(coxmod_list, ~tidy(., exponentiate = T, conf.int=T, conf.level=0.95)) %>% filter(term == a) %>%
+  mutate_if(is.numeric, ~round(.,2)) %>% unite(HR.CI, estimate, conf.low, conf.high, sep = "-") %>%
+  add_column(model = coxmod_names, .before = T)
 
 # Score as categories -------------------------------
 
 #score as categories (4 total)
 coxmod1.2 <- coxph(Surv(duree_suivi1, ksein) ~ score_cat, data=df.scores_all)
-#score categories, adjusting for education level
 coxmod2.2 <- coxph(Surv(duree_suivi1, ksein) ~ score_cat + bacfemme2, data=df.scores_all)
-#score categories, adjusting for education level and menopausal status
 coxmod3.2 <- coxph(Surv(duree_suivi1, ksein) ~ score_cat + bacfemme2 + statfin_dquest, data=df.scores_all)
 
-summary(coxmod1.2)
-summary(coxmod2.2)
-summary(coxmod3.2)
+coxmodcat_list <- list(coxmod1.2, coxmod2.2, coxmod3.2) #models with score categories - all participants
+coxmodcat_3names <- c("no adjustment variables", "no adjustment variables", "no adjustment variables", "+ education level", "+ education level", "+ education level", "+ menopause", "+ menopause","+ menopause")
+coxmodcat_1name <- c("no adjustment variables", "+ education level", "+ menopause")
+catlist <- c("score_cat1", "score_cat2", "score_cat3")
+catvalues <- c("score 2-3.75", "score 4-5.75", "score 6-8")
+
+# One table with all the score info on all the models with score as categories
+tablecox_cat0 <- map_df(coxmodcat_list, ~tidy(., exponentiate = T, conf.int=T, conf.level=0.95)) %>% filter(term %in% catlist) %>%
+  mutate_if(is.numeric, ~round(.,2)) %>% unite(HR.CI, estimate, conf.low, conf.high, sep = "-") %>% add_column(model = coxmodcat_3names, .before = T)
+
+# Creatinga table with relevant data for the last model (coxmod3.2)
+tablecox_lastmodel <- tidy(coxmod3.2, exponentiate = T, conf.int=T, conf.level=0.95) %>% filter(term %in% catlist) %>% 
+  mutate_if(is.numeric, ~round(.,2)) %>% unite(HR.CI, estimate, conf.low, conf.high, sep = "-") %>% add_column(model = catvalues, .before = T) %>% select(-term)
+
