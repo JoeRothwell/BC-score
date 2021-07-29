feffa00c4d81b9ab9e8ad983bdcd1a8dd05e5375
SuzanneCl <suzanne.clougher@gmail.com>
2021-07-29 18:09:34 +0200

    changed histogram labels to french

--- E3N-graphs.R
+++ E3N-graphs.R
@@ -4,20 +4,23 @@ source("E3N-prep_data_calc_score.R")
 library(ggplot2)
 library(tidyverse)
 library(grid)
+library(corrplot)
 
 # Score distribution simple histogram
-hist(df.scores_all$score, xlab = "WCRF/AICR score", main = paste("Scores in the E3N cohort"), xlim=range(2, 8))
+hist(df.scores_all$score, xlab = "score WCRF/AICR", ylab = "Fréquence", xlim=range(1, 8), main = NULL)
 
 # Score distribution histogram, colors according to status case VS control
 # need to find variable indicating breast cancer
 ggplot(df.scores_all) +
-  aes(x = score, fill = as.factor(ksein), xmin = 2, xmax =8) +  # k=0=pas de cancer du sein, k=1=cancer du sein
+  aes(x = score, xmin = 2, xmax =8) +  # k=0=pas de cancer du sein, k=1=cancer du sein
   geom_histogram(alpha = 0.5, position = "identity", bins = 22) +
   labs(x = "WCRF/AICR score", title = "WCRF/AICR scores in the E3N cancer group") 
 
 # Score components correlations---------------------------------------------------------------------
 tabcor_all <- cor(table_components_all)
-corrplot(tabcor_all, tl.col = "black", type = "upper") #, title = "Score components correlations-E3N cohort")
+corrplot(tabcor_all,  type = "upper", tl.col="black", tl.srt=40,  title = "Corrélations entre les composantes du score", mar=c(0,0,1,0)) #, title = "Score components correlations-E3N cohort")
+ #tl.srt changes labels orientation, mar lowers the title
+
 
 #en français
 tableFR <- df.scores_all %>%
